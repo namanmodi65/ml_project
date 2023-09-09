672e41d4fcbc08e108dbb9ca86f795c09a78c0cb
Naman Jain <103953760+namanmodi65@users.noreply.github.com>
2023-09-09 22:04:59 +0530

    Data_transformation

--- src/components/data_ingenstion.py
+++ src/components/data_ingenstion.py
@@ -5,6 +5,9 @@ from src.exceptions import CustomException
 from src.logger import logging
 import pandas as pd
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
@@ -49,4 +52,8 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    # obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
